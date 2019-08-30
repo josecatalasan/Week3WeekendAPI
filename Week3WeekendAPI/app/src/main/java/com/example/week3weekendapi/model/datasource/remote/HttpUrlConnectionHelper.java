65f726860b0cc3d276548bc1e6967ed187febdd0
Jose Catalasan <jose.catalasan@mobileappscompany.us>
2019-08-30 15:06:11 -0400

    Update HttpUrlConnectionHelper.java

--- Week3WeekendAPI/app/src/main/java/com/example/week3weekendapi/model/datasource/remote/HttpUrlConnectionHelper.java
+++ Week3WeekendAPI/app/src/main/java/com/example/week3weekendapi/model/datasource/remote/HttpUrlConnectionHelper.java
@@ -6,8 +6,12 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class HttpUrlConnectionHelper {
+    public static final String TOKEN = "5c1c523e5d563b8d5bf97d10c1e30ec3fea95b0e";
     public static final String MY_USER_PROFILE = "https://api.github.com/users/josecatalasan";
     public static final String MY_REPO_LIST = "https://api.github.com/users/josecatalasan/repos";
+    public static final String PROF_URL = "https://" + TOKEN + ":x-oauth-basic@" + MY_USER_PROFILE;
+    public static final String REPO_URL = "https://" + TOKEN + ":x-oauth-basic@" + MY_REPO_LIST;
+
     private static HttpURLConnection httpURLConnection;
     private static URL url;
 
